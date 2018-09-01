808f16ed4ea67637b418d3a0c5e4f6edae1cfbed
americanmouths <shiverr@gmail.com>
2018-09-01 16:51:46 -0700

    scope routes behind /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :services
-  resources :addresses
-  resources :owners
-  resources :pets
+  scope '/api' do
+    resources :services
+    resources :addresses
+    resources :owners
+    resources :pets
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
