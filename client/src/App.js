9090b4442a29a8c3243bedca950ebe9189b20f99
americanmouths <shiverr@gmail.com>
2018-09-08 13:45:12 -0700

    start building out react router, starting w/login page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 import './styles/global.js';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import PetsContainer from './components/PetsContainer';
 import NavBar from './containers/NavBar';
+import LogInPage from './containers/LogInPage';
 
 class App extends Component {
   render() {
@@ -13,6 +14,9 @@ class App extends Component {
           <NavBar />
           <h1>Sit/Stay</h1>
           <PetsContainer />
+          <Switch>
+            <Route exact path="/login" component={LogInPage}>
+          </Switch>
         </div>
       </Router>
     );
