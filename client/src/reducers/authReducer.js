2570c197e58a9f9deb6970bc57cf564f999c28c3
americanmouths <shiverr@gmail.com>
2018-09-08 13:53:25 -0700

    start auth reducer, add to root reducer

---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,29 @@
+export function authReducer(state = {
+  currentUser: {},
+  token: null,
+  loading: false,
+  errors: {}
+}, action) {
+  switch (action.type) {
+    case 'LOADING_AUTH_REQ':
+      return {...state, loading: true}
+    case 'LOG_IN':
+      return setUser(state, action.payload)
+    default:
+      return state;
+  }
+};
+
+function setUser(state, user) {
+  if (user.token) {
+    localStorage.setItem('token', user.token);
+    localStorage.setItem('username', user.user.username);
+    localStorage.setItem('id', user.user.id);
+  }
+  return {...state,
+    currentUser: user.user,
+    token: user.token,
+    errors: user.errors,
+    loading: false
+  };
+}
