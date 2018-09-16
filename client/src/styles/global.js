05980960fb43f52bc5da8949219fae34134ce983
americanmouths <shiverr@gmail.com>
2018-09-16 14:55:38 -0700

    change styling

--- client/src/styles/global.js
+++ client/src/styles/global.js
@@ -4,8 +4,8 @@ import { injectGlobal } from 'styled-components'
 mintgreen: #95cfb7;
 whitish blue: #edf7f3;
 blue: #95cacf;
-pinkish: #cf95ad;
-link-hover: #6c9599;
+gray: #686e77;
+light-gray: #757C83;
 */
 
 injectGlobal`
@@ -14,12 +14,13 @@ injectGlobal`
     padding: 0;
     margin: 0;
     font-family: 'Lato', sans-serif;
-    color: #cf95ad;
+    color: #686e77;
     background: #edf7f3;
   }
   .navbar-default {
-    background: #95cfb7;;
-    border-bottom: 1px solid white;
+    background-image: linear-gradient(#95cfb7, #edf7f3);
+    border: none;
+    padding-bottom: 70px;
   }
   .navbar-default .navbar-brand {
     color: white;
@@ -30,17 +31,34 @@ injectGlobal`
     color: white;
   }
   .navbar-default .navbar-nav>li>a {
-    color: #edf7f3;
+    color: #757C83;
     font-size: 1em;
   }
-  .navbar-default .navbar-nav>li>a:hover {
-      color: #6c9599;
+  .navbar-default .navbar-nav>li>a:hover,
+  .navbar-default .navbar-nav>li>a:focus,
+  .navbar-default .navbar-nav>li>a:active{
+      color: #edf7f3;
   }
   .navbar-default .navbar-toggle {
-    border-color: #95cfb7;
+    border: none;
   }
   .navbar-default .navbar-toggle .icon-bar {
-    background-color: #cf95ad;
+    background-color: #757C83;
+    border: none;
+  }
+  .dropdown-menu>li>a {
+    color: #757C83;
+  }
+  .dropdown-menu>li>a:hover {
+    background: #95cacf;
+  }
+  .navbar-default .navbar-nav>.open>a, .navbar-default .navbar-nav>.open>a:focus, .navbar-default .navbar-nav>.open>a:hover {
+    color: #edf7f3;
+    background: none;
+  }
+  .navbar-default .navbar-nav>.active>a, .navbar-default .navbar-nav>.active>a:focus, .navbar-default .navbar-nav>.active>a:hover {
+    color: #757C83;;
+    background: none;
   }
   .carousel-control{
     background-image: none;
@@ -51,14 +69,16 @@ injectGlobal`
     background-image: none;
   }
   .jumbotron{
+    margin-top: 20px;
     width: 75%;
     margin: auto;
-    background: #95cacf;
+    background: #95cfb7;
     color: #edf7f3;
-    border-radius: 10px 70px; 
+    border: 1px solid #757C83;
+    border-radius: 20px 20px;
   }
   span.glyphicon{
-    color: #cf95ad;
+    color: #686e77;
   }
 `
 /*  .navbar-brand {
