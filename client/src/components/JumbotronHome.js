25a3e2f0cc4b04b19892eda6d65c926d3c78c8a7
americanmouths <shiverr@gmail.com>
2018-09-16 13:16:28 -0700

    create jumbotron home to display to non users

---
+++ client/src/components/JumbotronHome.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Jumbotron, Button, Carousel, ListGroup, ListGroupItem } from 'react-bootstrap';
+
+const JumbotronHome = () => {
+
+  return (
+    <div>
+    <Carousel>
+      <Carousel.Item>
+        <Jumbotron>
+          <center>
+            <h1>Welcome to Sit/Stay</h1>
+            <p>
+              This is free application for scheduling services for your pets.
+            </p>
+              Our services include:
+            <p>
+              <Button bsStyle="primary">Learn more</Button>
+            </p>
+          </center>
+        </Jumbotron>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Jumbotron>
+          <center>
+            <h1>Pet Care Your Way</h1>
+            <p>
+              Here at Sit/Stay we are dedicated to caring for your pets how you want.
+            </p>
+            <p>
+              <Button bsStyle="primary">Learn more</Button>
+            </p>
+          </center>
+        </Jumbotron>
+      </Carousel.Item>
+      <Carousel.Item>
+        <Jumbotron>
+          <center>
+            <h1>Sign Up or Login</h1>
+            <p>
+              To get started, choose your option below.
+            </p>
+            <p>
+              <Button bsStyle="primary">Learn more</Button>
+            </p>
+          </center>
+        </Jumbotron>
+      </Carousel.Item>
+    </Carousel>;
+  </div>
+  )
+}
+
+export default JumbotronHome;
