f6fc6ae4d07033e67cf406dd9f1cb0a076d2609a
americanmouths <shiverr@gmail.com>
2018-09-19 17:33:16 -0700

    start building out about page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import NavBar from './containers/NavBar';
 import LogInPage from './containers/LogInPage';
 import SignUpPage from './containers/SignUpPage';
 import Home from './components/Home';
+import About from './components/About';
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
           <NavBar />
           <Switch>
             <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
             <Route exact path="/signup" component={SignUpPage} />
             <Route exact path="/login" component={LogInPage} />
           </Switch>
---
+++ client/src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+const About = () => {
+
+  return (
+    <div>
+      <Jumbotron className="about-jumbotron">
+        <center>
+          <h1>Hi I&#39;m Kayla</h1>
+          <p>
+            Here at sit stay, we aim to provide quality services for all your pets needs.
+          </p>
+            Our services include:
+        </center>
+      </Jumbotron>
+    </div>
+    )
+  }
+
+export default About;
